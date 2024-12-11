c5bca4170a83b4c9ab29abc42b316a1aba3d1697
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-12-11 17:42:39 -0500

    adding context

---
+++ frontend/src/context/authContext.tsx
@@ -0,0 +1,53 @@
+import { ReactNode, SetStateAction, createContext, useEffect, useContext } from "react"
+
+type authUserType = {
+    id: string,
+    fullName: string, 
+    email: string,
+    profilePic: string,
+    gender: string
+}
+
+
+const AuthContext = createContext<{
+    authUser: authUserType | null;
+    setAuthUser: Dispath<SetStateAction<authUserType | null>>;
+    isLoading: boolean;
+}>({
+    authUser: null,
+    setAuthUser: () => {},
+    isLoading: true,
+});
+
+export const AuthContextProvider = ({children}:{children:ReactNode}) => {
+    const [authUser, setAuthUser] = useState<authUserType | null>(null)
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(async () => {
+        try {
+            const res = await fetch("/api/auth/me");
+            const data = await res.json();
+            if (!res.ok) {
+                throw new Error(data.message);
+            }
+            setAuthUser(data);
+            
+        } catch (error : any) {
+            console.error(error)
+        }finally{
+            setIsLoading(false);
+        }
+    })
+    return (
+        <AuthContext.Provider value = {{
+            authUser,
+            isLoading,
+            setAuthUser
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+}
